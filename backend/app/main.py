3265539f832c786c868097e567ab7ea5819c7af8
Fernando <fernandoesc180@gmail.com>
2025-10-04 12:33:39 -0600

    Limpieza

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes_simulation import router as sim_router
-from app.api.routes_nasa import router as nasa_router
 
 app = FastAPI(title="Meteor Impact API")
 
