d97a963a97f44064cd3eaa385743991377f6d909
Fernando Escobar Jaramillo <fernandoesc180@gmail.com>
2025-10-04 21:26:36 -0600

    Update main.py
    Implementacion basica de middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,17 @@ from app.api.routes_nasa import router as nasa_router
 
 app = FastAPI(title="Meteor Impact API")
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials=True,
+    allow_methods={"*"},
+    allow_headers=["*"]
+)
+
+
 @app.get("/Meteors Madness")
 def health():
     return {"ok": True}
